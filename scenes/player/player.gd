914744c579a2d2134ad0a4f7b7a595c3bb018bb4
Ghislain Thiery <ghislain.thiery@ntymail.com>
2025-07-31 12:56:46 +0200

    main scene + basic movement

---
+++ scenes/player/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 7
+
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		if (velocity.y * -1) > SPEED / 2:
+			velocity.y = - SPEED / 2
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_up"):
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	if input_dir.y > 0:
+		input_dir.y = 0
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		#velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
