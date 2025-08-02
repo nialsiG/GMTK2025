c98f7f7bbfc32e1ae7db96207b909b92341bc17f
Ghislain Thiery <ghislain.thiery@ntymail.com>
2025-08-02 22:58:10 +0200

    spooky env + basic menu

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,1 +1,32 @@
 extends Node
+
+@onready var start_menu = %StartMenu
+@onready var options_menu = %OptionsMenu
+@onready var info_screen = %InfoScreen
+@onready var menu_camera = %Menu_Camera
+@onready var player = %Player
+@onready var menu_camera_3d: Camera3D = $"3C/Menu_Camera/Camera3D"
+@onready var player_camera_3d: Camera3D = $"3C/Player/Camera3D"
+
+func _unhandled_input(event):
+	if event is InputEventKey and start_menu.visible and !options_menu.visible:
+		StartGame()
+
+func _ready():
+	StartMenu()
+	SignalManager.GameOver.connect(StartMenu)
+
+func StartGame():
+	start_menu.visible = false
+	menu_camera.visible = false
+	player_camera_3d.make_current()
+	player.visible = true
+
+func StartMenu():
+	start_menu.visible = true
+	menu_camera.visible = true
+	menu_camera_3d.make_current()
+	player.visible = false
+
+func _on_options_button_pressed():
+	options_menu.visible = !options_menu.visible
--- scenes/procedural_generation/river_chunk.gd
+++ scenes/procedural_generation/river_chunk.gd
@@ -4,11 +4,14 @@ class_name RiverChunk
 const SPEED = 2
 
 @onready var dimetrodon_prefab = load("res://scenes/obstacles/obstacle_dimetrodon.tscn")
+@onready var is_active: bool = true
 
 func _process(delta):
 	position += Vector3 (0, 0, SPEED * delta)
-	if position.z >= 5:
+	if position.z >= 5 and is_active:
 		SignalManager.NewRiverChunk.emit(Vector3(0,0,-10))
+		is_active = false
+	if !is_active:
 		await get_tree().create_timer(0.05).timeout
 		queue_free()
 
